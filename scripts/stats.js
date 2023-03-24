2c98d317249ae6d6715d064220d0c198949c46c7
Gaurav Kale <kalegd@dukes.jmu.edu>
2023-03-24 15:13:46 -0600

    Include referrerPolicy in stats.js

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,1 +1,4 @@
-fetch('https://api.digitalbacon.io/stats', {method: "POST"});
+fetch('https://api.digitalbacon.io/stats', {
+    method: 'POST',
+    referrerPolicy: 'no-referrer-when-downgrade',
+});
